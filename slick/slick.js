d81eb81669f6101cea319aa7b28bedef1423922f
yann dardot <yann@donkeycode.com>
2017-12-12 15:30:30 +0100

    ssr

--- slick/slick.js
+++ slick/slick.js
@@ -16,16 +16,22 @@
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
 ;(function(factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
+    if(typeof window === 'undefined') {
+      return;
     }
-
-}(function($) {
+      'use strict';
+      if (typeof define === 'function' && define.amd) {
+          define(['jquery'], factory);
+      } else if (typeof exports !== 'undefined') {
+          module.exports = factory(require('jquery'));
+      } else {
+          factory(jQuery);
+      }
+  
+  }(function($) {
+    if(typeof window === 'undefined') {
+      return;
+    }
     'use strict';
     var Slick = window.Slick || {};
 
